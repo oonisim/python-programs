Numerical gradient issue looked after. Fixed the logic to prevent floating precision error/unstability.

nlp/src/common/constants.py
@@ -10,7 +10,7 @@ import numpy as np
 # OFFSET_DELTA = 1e-10
 # OFFSET_LOG = (OFFSET_DELTA + 1e-7)  # Avoid log(0) -> inf by log(0+offset)
 # --------------------------------------------------------------------------------
-OFFSET_DELTA = 1e-8
+OFFSET_DELTA = 1e-9
 OFFSET_LOG = 1e-7      # Avoid log(0) -> inf by log(x) where x > offset
 
 # --------------------------------------------------------------------------------
@@ -21,9 +21,11 @@ OFFSET_LOG = 1e-7      # Avoid log(0) -> inf by log(x) where x > offset
 # Before it starts, need to stop X from getting closer to log(k).
 # Hence X < -np.log(OFFSET_LOG) * safety_margin_ratio.
 # --------------------------------------------------------------------------------
-BOUNDARY_SIGMOID = -np.log(OFFSET_LOG) * 0.8    # give 10% slack
+BOUNDARY_SIGMOID = -np.log(OFFSET_LOG) * 0.5
 
 # Min difference between f(x+h) and f(x-h) at numerical gradient to avoid
 # floating precision error. If f(x+h) - f(x-h) is small
-MIN_DIFF_AT_GN = OFFSET_DELTA ** 2
+MIN_DIFF_AT_GN = np.power(OFFSET_DELTA, 2)
+GN_DIFF_ACCEPTANCE_VALUE = OFFSET_DELTA / (10 ** 4)     # 1/10000
+GN_DIFF_ACCEPTANCE_RATIO = 1e-12
 assert np.isfinite(MIN_DIFF_AT_GN)

nlp/src/common/functions.py
@@ -16,7 +16,8 @@ from common import (
     OFFSET_DELTA,
     OFFSET_LOG,
     BOUNDARY_SIGMOID,
-    MIN_DIFF_AT_GN
+    GN_DIFF_ACCEPTANCE_VALUE,
+    GN_DIFF_ACCEPTANCE_RATIO
 )
 Logger = logging.getLogger("functions")
 Logger.setLevel(logging.DEBUG)
@@ -505,6 +506,13 @@ def numerical_jacobian(
     Jacobian matrix element Jpq = df/dXpq, the impact on J by the
     small difference to Xpq where p is row index and q is col index of J.
 
+    Note:
+        Beware limitations by the float storage size, e.g. loss of significance.
+        https://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
+        https://ece.uwaterloo.ca/~dwharder/NumericalAnalysis/Contents/
+        https://ece.uwaterloo.ca/~dwharder/NumericalAnalysis/02Numerics/Weaknesses/
+        https://www.cise.ufl.edu/~mssz/CompOrg/CDA-arith.html
+
     Args:
         f: Y=f(X) where Y is a scalar or shape() array.
         X: input of shame (N, M), or (N,) or ()
@@ -546,7 +554,7 @@ def numerical_jacobian(
         assert \
             ((isinstance(fx1, np.ndarray) and fx1.size == 1) or isinstance(fx1, float)), \
             "The f function needs to return scalar or shape () but %s" % fx1
-        assert np.all(np.isfinite(fx1)), \
+        assert np.isfinite(fx1), \
             "f(x+h) caused nan for f %s for X %s" % (f, (tmp + delta))
 
         # --------------------------------------------------------------------------------
@@ -561,28 +569,41 @@ def numerical_jacobian(
         assert \
             ((isinstance(fx2, np.ndarray) and fx2.size == 1) or isinstance(fx2, float)), \
             "The f function needs to return scalar or shape () but %s" % fx2
-        assert np.all(np.isfinite(fx2)), \
+        assert np.isfinite(fx2), \
             "f(x-h) caused nan for f %s for X %s" % (f, (tmp - delta))
 
         # --------------------------------------------------------------------------------
-        # Subtraction between f(x+k) f(x-k) when they are very close is too small to f(x)
-        # Then the error of 1/P relative to f(x+k) and f(x-k) is much larger relative to
-        # f(x+k)-f(x-k), and the result is unstable/unreliable.
-        # See https://stackoverflow.com/a/66399722/4281353
+        # When f(x+k) and f(x-k) are relatively too close, subtract between them can ben
+        # too small, and the precision error 1/f(x) relative to f(x+k) and f(x-k) is much
+        # larger relative to that of f(x+k) or f(x-k), hence the result can be unstable.
+        # Prevent the subtract df(x) from being too small to f(x) by assuring df(x)/dx is
+        # greater than GN_DIFF_ACCEPTANCE_RATIO.
+        #
+        # e.g. For logistic log loss function f(x) with log(+1e-7) to avoid log(0)/inf.
+        # x[14.708627877981929] (x+h)[14.708627878981929] fx1=[14.708628288297405]
+        # x[14.708627877981929] (x-h)[14.708627876981929] fx2=[14.708628286670217]
+        # (fx1-fx2)=[1.6271872738116144e-09]
+        # (fx1-fx2) / fxn < 1e-10. The difference is relatively too small to f(x).
+        #
         #
         # If the gradient of f(x) at x is nearly zero, or saturation, then reconsider
-        # if using the numerical gradient is fit for the purpose.
+        # if using the numerical gradient is fit for the purpose. Prevent the gradient
+        # from being too close to zero by f(x+k)-f(x-k) > GN_DIFF_ACCEPTANCE_VALUE
         # --------------------------------------------------------------------------------
         Logger.debug("%s: (fx1-fx2)=[%s]", name, (fx1-fx2))
-        assert np.abs(fx1 - fx2) > MIN_DIFF_AT_GN, \
-            "Need (fx1 - fx2) > %s to avoid float error but %s." \
-            % (MIN_DIFF_AT_GN, np.abs(fx1 - fx2))
+        difference = (fx1 - fx2)
+        assert \
+            (np.abs(difference) > GN_DIFF_ACCEPTANCE_VALUE) and \
+            (np.abs(difference) > (fx1 * GN_DIFF_ACCEPTANCE_RATIO)) and \
+            (np.abs(difference) > (fx2 * GN_DIFF_ACCEPTANCE_RATIO)), \
+            "Need (fx1-fx2) / fxn > %s to avoid float error but %s. GN is %s" \
+            % (GN_DIFF_ACCEPTANCE_RATIO, np.abs(difference), (fx1-fx2) / (2 * delta))
 
         # --------------------------------------------------------------------------------
         # Set the gradient element scalar value or shape()
         # --------------------------------------------------------------------------------
-        g: Union[np.ndarray, float] = (fx1 - fx2) / (2 * delta)
-        assert np.all(np.isfinite(g))
+        g: Union[np.ndarray, float] = np.subtract(fx1, fx2) / (2 * delta)
+        assert np.isfinite(g)
         J[idx] = g
 
         Logger.debug("%s: idx[%s] j=[%s]", name, idx, g)

nlp/src/layer/test_040_objective_sigmoid.py
@@ -401,9 +401,10 @@ def test_040_objective_methods_1d_ohe():
         # constraint: Analytical gradient G is close to GN: gradient_numerical().
         # --------------------------------------------------------------------------------
         assert \
-            np.all(np.abs(G - GN[0]) <= GRADIENT_DIFF_ACCEPTANCE_VALUE) or \
-            np.all(np.abs(G - GN[0]) <= np.abs(GRADIENT_DIFF_ACCEPTANCE_RATIO * GN[0])), \
-            f"dX is \n{G}\nGN[0] is \n{GN[0]}\nRatio * GN[0] is \n{GRADIENT_DIFF_ACCEPTANCE_RATIO * GN[0]}.\n"
+            np.all(np.abs(G-GN[0]) <= GRADIENT_DIFF_ACCEPTANCE_VALUE) or \
+            np.all(np.abs(G-GN[0]) <= np.abs(GRADIENT_DIFF_ACCEPTANCE_RATIO * GN[0])), \
+            "dX is \n%s\nGN is \n%s\nG-GN is \n%s\n Ratio * GN[0] is \n%s.\n" \
+            % (G, GN[0], G-GN[0], GRADIENT_DIFF_ACCEPTANCE_RATIO * GN[0])
 
 
 def test_040_objective_methods_2d_ohe():