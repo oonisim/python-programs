Numerical gradient issue looked after. Fixed the logic to prevent floating precision error/unstability.

nlp/src/common/constants.py
@@ -23,9 +23,10 @@ OFFSET_LOG = 1e-7      # Avoid log(0) -> inf by log(x) where x > offset
 # --------------------------------------------------------------------------------
 BOUNDARY_SIGMOID = -np.log(OFFSET_LOG) * 0.5
 
+# Threshold below which the gradient is regarded as saturated.
+GRADIENT_SATURATION_THRESHOLD = (10 ** -4)     # 1/10000
+
 # Min difference between f(x+h) and f(x-h) at numerical gradient to avoid
 # floating precision error. If f(x+h) - f(x-h) is small
-MIN_DIFF_AT_GN = np.power(OFFSET_DELTA, 2)
-GN_DIFF_ACCEPTANCE_VALUE = OFFSET_DELTA / (10 ** 4)     # 1/10000
+GN_DIFF_ACCEPTANCE_VALUE = 2 * OFFSET_DELTA * GRADIENT_SATURATION_THRESHOLD
 GN_DIFF_ACCEPTANCE_RATIO = 1e-12
-assert np.isfinite(MIN_DIFF_AT_GN)

nlp/src/common/functions.py
@@ -17,7 +17,8 @@ from common import (
     OFFSET_LOG,
     BOUNDARY_SIGMOID,
     GN_DIFF_ACCEPTANCE_VALUE,
-    GN_DIFF_ACCEPTANCE_RATIO
+    GN_DIFF_ACCEPTANCE_RATIO,
+    GRADIENT_SATURATION_THRESHOLD
 )
 Logger = logging.getLogger("functions")
 Logger.setLevel(logging.DEBUG)
@@ -593,7 +594,6 @@ def numerical_jacobian(
         Logger.debug("%s: (fx1-fx2)=[%s]", name, (fx1-fx2))
         difference = (fx1 - fx2)
         assert \
-            (np.abs(difference) > GN_DIFF_ACCEPTANCE_VALUE) and \
             (np.abs(difference) > (fx1 * GN_DIFF_ACCEPTANCE_RATIO)) and \
             (np.abs(difference) > (fx2 * GN_DIFF_ACCEPTANCE_RATIO)), \
             "Need (fx1-fx2) / fxn > %s to avoid float error but %s. GN is %s" \
@@ -604,6 +604,10 @@ def numerical_jacobian(
         # --------------------------------------------------------------------------------
         g: Union[np.ndarray, float] = np.subtract(fx1, fx2) / (2 * delta)
         assert np.isfinite(g)
+        assert \
+            np.abs(g) > GRADIENT_SATURATION_THRESHOLD, \
+            "The gradient [%s] may have been saturated." % g
+
         J[idx] = g
 
         Logger.debug("%s: idx[%s] j=[%s]", name, idx, g)

nlp/src/layer/base.py
@@ -110,7 +110,8 @@ from typing import (
 import inspect
 import logging
 import numpy as np
-from common.functions import (
+from common import (
+    GRADIENT_SATURATION_THRESHOLD,
     numerical_jacobian
 )
 
@@ -203,6 +204,9 @@ class Layer:
         assert X is not None and \
                ((isinstance(X, np.ndarray) and X.dtype == float) or isinstance(X, float))
 
+        if np.all(np.abs(X) > 1.0):
+            self.logger.warning("Input data X has not been standardized.")
+
         if isinstance(X, float):
             self._X = np.array(X)
         elif X.ndim == 1:
@@ -228,6 +232,12 @@ class Layer:
         """Gradient dL/dX"""
         assert isinstance(self._dX, np.ndarray) and self._dX.size > 0, \
             "dX is not initialized"
+
+        if np.all(np.abs(self._dX) < GRADIENT_SATURATION_THRESHOLD):
+            self.logger.warning(
+                "Gradient dL/dX \n[%s] may have been saturated." % self._dX
+            )
+
         return self._dX
 
     @property
@@ -262,6 +272,11 @@ class Layer:
         """Latest gradient dL/dY (impact on L by dY) given from the post layer(s)"""
         assert isinstance(self._dY, np.ndarray) and self._dY.dtype == float \
             and self._dY.size > 0, "dY is not initialized or invalid"
+
+        if np.all(np.abs(self._dY) < GRADIENT_SATURATION_THRESHOLD):
+            self.logger.warning(
+                "Gradient dY/dX \n[%s] may have been saturated." % self._dY
+            )
         return self._dY
 
     @property