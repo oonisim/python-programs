4a589116a6ff9046880f6105721855ae8bcf9dd7
oonisim <oonisim@gmail.com>
2021-03-15 06:40:35 +1100

    Typo fix.

--- nlp/src/drawing/functions.py
+++ nlp/src/drawing/functions.py
@@ -54,7 +54,7 @@ def plot_categorical_predictions(axes, grid, X, Y, predictions):
 
     # plot predictions
     # axes.contourf(grid[0], grid[1], predictions, cmap=plt.cm.Spectral, alpha=0.3)
-    axes.contourf(grid[0], grid[1], predictions, cmap=plt.cm.gist_rainbow, alpha=0.3, linewidths=10)
+    axes.contourf(grid[0], grid[1], predictions, cmap=plt.cm.gist_rainbow, alpha=0.4, linewidths=3.0)
 
     axes.set_xlim(grid[0].min(), grid[0].max())
     axes.set_ylim(grid[1].min(), grid[1].max())
