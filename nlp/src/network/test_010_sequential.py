bb708ada8fa4631c72cb32731cf851685e756a1d
oonisim <oonisim@gmail.com>
2021-03-29 01:05:19 +1100

    Included BatchNormalization and more hidden layers

--- nlp/src/network/test_010_sequential.py
+++ nlp/src/network/test_010_sequential.py
@@ -253,7 +253,7 @@ def test_010_sequential_train():
     X, T = transform_X_T(X, T)
     assert X.shape == (N, D)
 
-    num_epochs = 5
+    num_epochs = 1000
 
     loss.T = T
     for i in range(num_epochs):
@@ -298,5 +298,165 @@ def test_010_sequential_train():
         # --------------------------------------------------------------------------------
         matmul.update()  # dL/dX, dL/dW
         dS = network.update()
-        print(f"dS is {dS}")
 
+        print(L_sequential)
+
+
+def test_010_sequential_train2():
+    # --------------------------------------------------------------------------------
+    # Network without sequential layer
+    # --------------------------------------------------------------------------------
+    N = 16
+    D = 2
+    M: int = 3
+    lr = 0.1
+    l2 = 1e-3
+    num_epochs = 100
+
+    # --------------------------------------------------------------------------------
+    # Network using sequential
+    # --------------------------------------------------------------------------------
+    valid_network_specification = {
+        "matmul01": {
+            _SCHEME: layer.Matmul.__qualname__,
+            _PARAMETERS: {
+                _NUM_NODES: 16,
+                _NUM_FEATURES: 2,  # NOT including bias
+                _WEIGHTS: {
+                    _SCHEME: "he"
+                },
+                _OPTIMIZER: {
+                    _SCHEME: optimiser.SGD.__qualname__,
+                    _PARAMETERS: {
+                        "lr": lr,
+                        "l2": l2
+                    }
+                }
+            },
+        },
+        "bn01": {
+            _SCHEME: layer.BatchNormalization.__qualname__,
+            _PARAMETERS: {
+                _NUM_NODES: 16,
+                "momentum": 0.9,
+            }
+        },
+        "activation01": {
+            _SCHEME: layer.ReLU.__qualname__,
+            _PARAMETERS: {
+                _NUM_NODES: 16
+            }
+        },
+        "matmul02": {
+            _SCHEME: layer.Matmul.__qualname__,
+            _PARAMETERS: {
+                _NUM_NODES: 32,
+                _NUM_FEATURES: 16,  # NOT including bias
+                _WEIGHTS: {
+                    _SCHEME: "he"
+                },
+                _OPTIMIZER: {
+                    _SCHEME: optimiser.SGD.__qualname__,
+                    _PARAMETERS: {
+                        "lr": lr,
+                        "l2": l2
+                    }
+                }
+            },
+        },
+        "bn02": {
+            _SCHEME: layer.BatchNormalization.__qualname__,
+            _PARAMETERS: {
+                _NUM_NODES: 32,
+            }
+        },
+        "activation02": {
+            _SCHEME: layer.ReLU.__qualname__,
+            _PARAMETERS: {
+                _NUM_NODES: 32
+            }
+        },
+        "matmul03": {
+            _SCHEME: layer.Matmul.__qualname__,
+            _PARAMETERS: {
+                _NUM_NODES: 16,
+                _NUM_FEATURES: 32,  # NOT including bias
+                _WEIGHTS: {
+                    _SCHEME: "he"
+                },
+                _OPTIMIZER: {
+                    _SCHEME: optimiser.SGD.__qualname__,
+                    _PARAMETERS: {
+                        "lr": lr,
+                        "l2": l2
+                    }
+                }
+            },
+        },
+        "activation03": {
+            _SCHEME: layer.ReLU.__qualname__,
+            _PARAMETERS: {
+                _NUM_NODES: 16
+            }
+        },
+        "matmul04": {
+            _SCHEME: layer.Matmul.__qualname__,
+            _PARAMETERS: {
+                _NUM_NODES: 3,
+                _NUM_FEATURES: 16,  # NOT including bias
+                _WEIGHTS: {
+                    _SCHEME: "he"
+                }
+            }
+        },
+        "objective": {
+            _SCHEME: layer.CrossEntropyLogLoss.__qualname__,
+            _PARAMETERS: {
+                _NUM_NODES: M,
+                _LOSS_FUNCTION: "softmax_cross_entropy_log_loss"
+            }
+        }
+    }
+
+    # ----------------------------------------------------------------------
+    # Validate the correct specification.
+    # NOTE: Invalidate one parameter at a time from the correct one.
+    # Otherwise not sure what you are testing.
+    # ----------------------------------------------------------------------
+    try:
+        network = SequentialNetwork(
+            name="test_010_base_instantiation_to_fail",
+            num_nodes=M,    # number of the last layer output,
+            specification=valid_network_specification,
+            log_level=logging.DEBUG
+        )
+
+        inference_layer: layer.Sequential = network.layer_inference
+        sequential_matmul_layer: layer.Matmul = inference_layer.layers[0]
+        assert sequential_matmul_layer.optimizer.lr == lr
+        assert sequential_matmul_layer.optimizer.l2 == l2
+
+    except Exception as e:
+        raise RuntimeError(
+            "SequentialNetwork() must succeed with %s"
+            % valid_network_specification
+        )
+
+    # --------------------------------------------------------------------------------
+    # Training data
+    # --------------------------------------------------------------------------------
+    X, T, V = linear_separable_sectors(n=N, d=D, m=M)
+    X, T = transform_X_T(X, T)
+    assert X.shape == (N, D)
+
+    for i in range(num_epochs):
+        # --------------------------------------------------------------------------------
+        # Run sequential
+        # --------------------------------------------------------------------------------
+        network.T = T
+        A_sequential = network.function(X)
+        L_sequential = network.objective(A_sequential)
+        dX_sequential = network.gradient(TYPE_FLOAT(1))
+
+        dS = network.update()
+        print(L_sequential)
