17bac9eadcdd32c41561278caa00b9d6605e3573
oonisim <oonisim@gmail.com>
2021-03-31 17:14:21 +1100

    Sum under construction.

--- nlp/src/layer/sum.py
+++ nlp/src/layer/sum.py
@@ -44,14 +44,20 @@ class Sum(Layer):
     # Instance
     # ================================================================================
     @property
-    def is_averaged(self) -> int:
+    def is_averaged(self) -> bool:
         """Flat if the output Y is averaged by N"""
         return self._is_averaged
+
+    @property
+    def axis(self) -> int:
+        """axis to sum along"""
+        return self._axis
 
     def __init__(
             self,
             name: str,
             num_nodes: int,
+            axis: int = 0,
             is_averaged: bool = False,
             log_level: int = logging.ERROR
     ):
@@ -59,15 +65,20 @@ class Sum(Layer):
         Args:
             name: layer name
             num_nodes: number of nodes in the layer
+            axis: axis along which to sum
             is_averaged: flag to average the output
             log_level: logging level
         """
+        assert isinstance(axis, int) and axis == 0, "Currently axis=0 is only supported"
+        assert isinstance(is_averaged, bool)
+
         super().__init__(name=name, num_nodes=num_nodes, log_level=log_level)
 
         self.mask: np.ndarray = np.empty(())    # To zero clear the outputs where x <= 0
-        self._M = num_nodes                     # Number of nodes alias
+        self._M: int = num_nodes                     # Number of nodes alias
         self._shape_X = ()                      # Original shape of X
-        self._is_averaged = is_averaged
+        self._is_averaged: bool = is_averaged
+        self._axis: int = axis
 
     # --------------------------------------------------------------------------------
     # Instance methods
@@ -80,7 +91,7 @@ class Sum(Layer):
             shape_Y = X[0].shape if X.ndim > 0 else ()
             self._Y = np.empty(shape_Y, dtype=TYPE_FLOAT)
 
-        np.sum(X, axis=0, out=self._Y) \
+        np.sum(X, axis=self.axis, out=self._Y) \
             if self.is_averaged \
             else np.sum(X/self.N, out=self._Y)
 
@@ -94,6 +105,8 @@ class Sum(Layer):
             dY: dL/dY of shape (1,M) or (M,), impact on L by the activation dY.
         Returns:
             dL/dX: impact on L by the layer input dX
+
+        TODO: How to restore the shape when axis != 0. Need to insert np.newaxis?
         """
         dY = np.array(dY).reshape((1, -1)) if isinstance(dY, float) else dY
         assert dY.shape == self.Y.shape, \
