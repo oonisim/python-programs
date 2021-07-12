cb26091a0cf51b5a6d24a121834d25ff188029ba
oonisim <oonisim@gmail.com>
2021-07-12 17:58:38 +1000

    Reorganized structure.

--- huggingface/kaggle_toxic_comment_challenge/merge.sh
+++ huggingface/kaggle_toxic_comment_challenge/merge.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+DIR=$(realpath $(dirname $0))
+PARENT=$(realpath "${DIR}/..")
+
+cd ${DIR}/notebook
 files=($(ls | grep  '^[0-9][0-9]*.*\.ipynb' | sort -g))
 
 #--------------------------------------------------------------------------------
@@ -15,22 +19,28 @@ files=($(ls | grep  '^[0-9][0-9]*.*\.ipynb' | sort -g))
 #  10_model_history.ipynb 
 #  11_evaluation.ipynb
 #--------------------------------------------------------------------------------
+echo "#--------------------------------------------------------------------------------"
 echo "generating the merged notebook as toxic_comment_classification.ipynb"
-nbmerge "${files[@]}" > toxic_comment_classification.ipynb
+echo "#--------------------------------------------------------------------------------"
+nbmerge "${files[@]}" > ${DIR}/toxic_comment_classification.ipynb
 
-#--------------------------------------------------------------------------------
-# Generate a analysis notebook for a specific category
-#--------------------------------------------------------------------------------
-echo "generating the analysis notebook as toxic_comment_analysis.ipynb"
-nbmerge 01_setup.ipynb 02_analysis.ipynb > toxic_comment_analysis.ipynb
+echo "#--------------------------------------------------------------------------------"
+echo "# Generate the analysis notebook"
+echo "#--------------------------------------------------------------------------------"
+nbmerge 01_setup.ipynb 02_analysis.ipynb > ${DIR}/toxic_comment_analysis.ipynb
 
-#--------------------------------------------------------------------------------
-# Generate a training notebook for a specific category
-#--------------------------------------------------------------------------------
+echo "#--------------------------------------------------------------------------------"
+echo "# Generate the training notebook for a specific category"
+echo "#--------------------------------------------------------------------------------"
 CATEGORIES=("toxic" "severe" "obscene" "threat" "insult" "identity")
 for category in "${CATEGORIES[@]}"
 do
   echo "generating a model training notebook for $category"
   target=$(find -maxdepth 1 -type f -name "*_model_${category}.ipynb" -printf '%P\n'  | grep ${category})
-  nbmerge 01_setup.ipynb 03_model_setup.ipynb ${target} > toxic_comment_${category}.ipynb
+  nbmerge 01_setup.ipynb 03_model_setup.ipynb ${target} 11_evaluation.ipynb > ${DIR}/toxic_comment_${category}.ipynb
 done
+
+echo "#--------------------------------------------------------------------------------"
+echo "# Generate the evaluation notebook for a specific category"
+echo "#--------------------------------------------------------------------------------"
+nbmerge 01_setup.ipynb 03_model_setup.ipynb 11_evaluation.ipynb > ${DIR}/toxic_comment_evaluation.ipynb
