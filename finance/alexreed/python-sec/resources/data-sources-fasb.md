1b95322ee2d43e3ec86e53abcc1253a5994bb35a
oonisim <oonisim@gmail.com>
2022-01-01 20:30:19 +1100

    relocated finance to another repo

--- finance/alexreed/python-sec/resources/data-sources-fasb.md
+++
@@ -1,5 +0,0 @@
-# DATA DIRECTORIES - FASB
-
-**US GAAP Taxonomy Directory:**
-
-<http://xbrl.fasb.org/us-gaap/>
--- finance/alexreed/python-sec/samples/use_client.py
+++
@@ -1,7 +0,0 @@
-import pprint
-from datetime import date
-from edgar.client import EdgarClient
-from edgar.enums import FilingTypeCodes
-
-# Initalize the client.
-edgar_client = EdgarClient()
