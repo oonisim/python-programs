3ac08e9468f9d4ef4d4d103c8b74df904383c992
oonisim <oonisim@gmail.com>
2022-03-05 21:02:47 +1100

    Remove edg

--- edg/src/serving/test_000_.py
+++
@@ -1,8 +0,0 @@
-def test_instantiation():
-    """
-    Objective:
-        Verify the layer class validates the initialization parameter constraints.
-    Expected:
-        Initialization detects parameter constraints not meet and fails.
-    """
-    name = "test_020_matmul_instantiation_to_fail"
--- edg/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-docker build -t edg -f src/serving/Dockerfile .
-docker run --publish 8080:8080 edg
