a85fc01599ebf91407817ea7c1cd879bbe3ff36b
oonisim <oonisim@gmail.com>
2023-02-22 13:07:58 +1100

    YOLO v1

--- tensorflow/cnn/yolo/v1/src/loss.py
+++ tensorflow/cnn/yolo/v1/src/loss.py
@@ -58,7 +58,18 @@ _logger: logging.Logger = get_logger(__name__)
 # Layer
 # --------------------------------------------------------------------------------
 class YOLOLoss(Loss):
-    """YOLO v1 objective (loss) layer"""
+    """YOLO v1 objective (loss) layer
+    [References]
+    https://www.tensorflow.org/guide/keras/train_and_evaluate#custom_losses
+    If you need a loss function that takes in parameters beside T and y_pred,
+    you can subclass the tf.keras.losses.Loss class and implement the two methods:
+    1. __init__(self): accept parameters to pass during the call of the loss function
+    2. call(self, T, y_pred): compute the model's loss
+
+    https://www.tensorflow.org/api_docs/python/tf/keras/losses/Loss
+    > To be implemented by subclasses:
+    >     call(): Contains the logic for loss calculation using T, y_pred.
+    """
     def __init__(
             self,
             S: int = YOLO_GRID_SIZE,                # pylint: disable=invalid-name
@@ -75,7 +86,7 @@ class YOLOLoss(Loss):
             C: number of classes
             P: number of predictions per bounding box (cp, x, y, w, h)
         """
-        super().__init__(reduction=tf.keras.losses.Reduction)
+        super().__init__(reduction=tf.keras.losses.Reduction, **kwargs)
         self.N: int = -1    # Number of batch records
         self.S: int = S
         self.B: int = B
@@ -102,18 +113,10 @@ class YOLOLoss(Loss):
         self.lambda_coord: TYPE_FLOAT = TYPE_FLOAT(5.0)
         self.lambda_noobj: TYPE_FLOAT = TYPE_FLOAT(0.5)
 
-    def build(self, input_shape):
-        """build the layer state
-        Args:
-            input_shape: TensorShape, or list of instances of TensorShape
-        """
-        # Tell Keras the layer is built
-        super().build(input_shape=input_shape)
-
     def get_config(self) -> dict:
-        """
-        Return serializable layer configuration from which the layer can be reinstantiated.
-        https://www.tensorflow.org/api_docs/python/tf/keras/layers/Layer#get_config
+        """Returns the config dictionary for a Loss instance.
+        https://www.tensorflow.org/api_docs/python/tf/keras/losses/Loss#get_config
+        Return serializable layer configuration from which the instance can be reinstantiated.
         """
         config = super().get_config().copy()
         config.update({
@@ -127,54 +130,65 @@ class YOLOLoss(Loss):
         return config
 
     def call(
-            self, y_true: Union[np.ndarray, tf.Tensor], y_pred: Union[np.ndarray, tf.Tensor]
+            self,
+            T: Union[np.ndarray, tf.Tensor],
+            Y: Union[np.ndarray, tf.Tensor]
     ) -> tf.Tensor:
         """Loss forward process
-        https://www.tensorflow.org/api_docs/python/tf/keras/losses/Loss
+        See ../image/yolo_loss_function.png and the original v1 paper.
+        [Steps]
+        1. Take the max IoU per cell (from between b-boxes and the truth at each cell).
+        2. Get Iobj_i_j per each cell i.
+           IObj_i is 1 if the cell i is responsible (pc in truth==1) for an object, or 0.
+           IObj_i_j is 1 if IObj_i is 1 and bbox j has the max IoU at the cell, or 0.
+        3. Take sqrt(w) and sqrt(h) to reduce the impact from the object size.
+
+        [References]
+        https://www.tensorflow.org/api_docs/python/tf/keras/metrics/mean_squared_error
 
         Args:
-            y_true: ground truth
-            y_pred: prediction of shape (N, S, S, (C+B*P)) where N is batch size
+            T: ground truth
+            Y: prediction of shape (N, S, S, (C+B*P)) where N is batch size
 
         Returns: loss
         """
         # The model output shape for one input image should be
         # (S=7 * S=7 * (C+B*P)=30) or (S, S, (C+B*P)).
-        assert isinstance(y_pred, (np.ndarray, tf.Tensor, tf.Variable))
-        assert y_pred.shape[-1] in (
+        assert isinstance(Y, (np.ndarray, tf.Tensor, tf.Variable))
+        assert Y.shape[-1] in (
             (self.S * self.S * (self.C + self.B * self.P)),
             (self.C + self.B * self.P)
         )
 
         # The label shape for one input image should be
         # (S=7 * S=7 * (C+P)=25) or (S, S, (C+P)).
-        assert isinstance(y_true, (np.ndarray, tf.Tensor))
-        assert y_true.shape[-1] in (
+        assert isinstance(T, (np.ndarray, tf.Tensor))
+        assert T.shape[-1] in (
             (self.S * self.S * (self.C + self.P)),
             (self.C + self.P)
         )
 
         # --------------------------------------------------------------------------------
         # Convert to tf.Variable to be able to update it.
-        # y_pred into (N, S, S, (C+B*P)) of YOLO v1 model output.
-        # y_true into (N, S, S, (C+P)
+        # Y into (N, S, S, (C+B*P)) of YOLO v1 model output.
+        # T into (N, S, S, (C+P)
         # --------------------------------------------------------------------------------
-        y_pred: tf.Variable = tf.Variable(y_pred).reshape((-1, self.S, self.S, self.C + self.B * self.P))
-        y_true: tf.Tensor = tf.Variable(y_true).reshape((-1, self.S, self.S, self.C + self.P))
-        assert y_pred.shape[0] == y_true.shape[0]
-        self.N: tf.Tensor = y_pred.shape[0]
+        Y: tf.Variable = tf.Variable(Y).reshape((-1, self.S, self.S, self.C + self.B * self.P))
+        T: tf.Tensor = tf.Variable(T).reshape((-1, self.S, self.S, self.C + self.P))
+        assert Y.shape[0] == T.shape[0]
+        self.N: tf.Tensor = Y.shape[0]
         _logger.debug("%s: batch size [%s]", self.N)
 
         # --------------------------------------------------------------------------------
-        # IoU per predicted bounding box with target bbox
+        # IoU per predicted bounding box and target bbox
         # --------------------------------------------------------------------------------
         iou_b1 = intersection_over_union(
-            y_pred[..., YOLO_PREDICTION_INDEX_X1:YOLO_PREDICTION_INDEX_H1+1],   # (x,y,w,h) for the first bbox
-            y_true[..., YOLO_LABEL_INDEX_X:YOLO_LABEL_INDEX_H+1]                # (x,y,w,h) for label
+            Y[..., YOLO_PREDICTION_INDEX_X1:YOLO_PREDICTION_INDEX_H1+1],   # (x,y,w,h) for the first bbox
+            T[..., YOLO_LABEL_INDEX_X:YOLO_LABEL_INDEX_H+1]                # (x,y,w,h) for label
         )
         iou_b2 = intersection_over_union(
-            y_pred[..., YOLO_PREDICTION_INDEX_X2:YOLO_PREDICTION_INDEX_H2+1],
-            y_true[..., YOLO_LABEL_INDEX_X:YOLO_LABEL_INDEX_H+1]                # (x,y,w,h) for label
+            Y[..., YOLO_PREDICTION_INDEX_X2:YOLO_PREDICTION_INDEX_H2+1],
+            T[..., YOLO_LABEL_INDEX_X:YOLO_LABEL_INDEX_H+1]                # (x,y,w,h) for label
         )
         # --------------------------------------------------------------------------------
         # Max IOU per grid cell (axis=-1)
@@ -205,30 +219,33 @@ class YOLOLoss(Loss):
         # Note that the loss function only penalizes classification error if an object
         # is present in that grid cell (hence the conditional class probability discussed).
         # --------------------------------------------------------------------------------
-        identity_i: tf.Tensor = y_true[..., YOLO_LABEL_INDEX_CP:YOLO_LABEL_INDEX_CP+1]
-        assert identity_i.shape == (self.N, self.S, self.S, 1), \
-            f"expected shape {(self.N, self.S, self.S, 1)} got {identity_i.shape}."
+        Iobj_i: tf.Tensor = T[..., YOLO_LABEL_INDEX_CP:YOLO_LABEL_INDEX_CP+1]
+        assert Iobj_i.shape == (self.N, self.S, self.S, 1), \
+            f"expected shape {(self.N, self.S, self.S, 1)} got {Iobj_i.shape}."
 
         # --------------------------------------------------------------------------------
-        # Predicted localization (x, y, w, h) from the best bounding box per grid cell.
-        # This corresponds to Iobj_i_j as picking up the best box j for each cell.
+        # Predicted localization (x, y, w, h) from the best bounding box j per grid cell.
+        # This corresponds to Iobj_i_j as picking up the best box j at each cell.
         # if best box j == 0, then YOLO_PREDICTION_INDEX_X1:YOLO_PREDICTION_INDEX_H1+1 as
         # the (x, y, w, h) for the predicted localization. If j == 1, the other.
         # --------------------------------------------------------------------------------
-        boxes_predicted: tf.Tensor = identity_i * (
-            (TYPE_FLOAT(1.0) - best_box_j) * y_pred[..., YOLO_PREDICTION_INDEX_X1:YOLO_PREDICTION_INDEX_H1+1] +
-            best_box_j * y_pred[..., YOLO_PREDICTION_INDEX_X2:YOLO_PREDICTION_INDEX_H2+1]
+        boxes_predicted: tf.Tensor = Iobj_i * (
+            (TYPE_FLOAT(1.0) - best_box_j) * Y[..., YOLO_PREDICTION_INDEX_X1:YOLO_PREDICTION_INDEX_H1+1] +
+            best_box_j * Y[..., YOLO_PREDICTION_INDEX_X2:YOLO_PREDICTION_INDEX_H2+1]
         )
 
         # --------------------------------------------------------------------------------
-        # True localization (x, y, w, h) from per grid cell.
+        # True localization (x, y, w, h) per grid cell.
         # --------------------------------------------------------------------------------
-        boxes_truth: tf.Tensor = identity_i * y_true[YOLO_LABEL_INDEX_X:YOLO_LABEL_INDEX_H+1]
+        boxes_truth: tf.Tensor = Iobj_i * T[YOLO_LABEL_INDEX_X:YOLO_LABEL_INDEX_H+1]
         assert YOLO_PREDICTION_INDEX_H1-YOLO_PREDICTION_INDEX_X1 == 4
         assert YOLO_LABEL_INDEX_H-YOLO_LABEL_INDEX_X == 4
 
         # --------------------------------------------------------------------------------
-        # Take square root of w, h
+        # Take square root of w, h.
+        # https://datascience.stackexchange.com/questions/118674
+        # https://youtu.be/n9_XyCGr-MI?list=PLhhyoLH6Ijfw0TpCTVTNk42NN08H6UvNq&t=2804
+        # --------------------------------------------------------------------------------
         # [Original yolo v1 paper]
         # Sum-squared error also equally weights errors in large boxes and small boxes.
         # Our error metric should reflect that small deviations in large boxes matter
@@ -239,13 +256,30 @@ class YOLOLoss(Loss):
         w_h_predicted = boxes_predicted[..., 2:4]
         w_h_predicted.assign(
             # --------------------------------------------------------------------------------
-            # abs() as predicted values can be negative. EPSILON to avoid the derivative of
+            # abs(x) as predicted values can be negative. EPSILON to avoid the derivative of
             # sqrt(x), which is 0.5 * 1/sqrt(x), from getting infinitive when x == 0.
+            # sign(x) to restore the sign lost due to abs(x).
             # --------------------------------------------------------------------------------
             tf.math.sign(w_h_predicted) * tf.math.sqrt(tf.math.abs(w_h_predicted) + EPSILON)
         )
-        boxes_truth[..., 2:4].assign(
-            tf.math.sqrt(tf.math.abs(boxes_truth[..., 2:4]) + EPSILON)
-        )
+        # (x/0, y/1, w/2, h/3)
+        w_h_truth = boxes_truth[..., 2:4]
+        w_h_truth.assign(tf.math.sqrt(w_h_truth))
 
-        return tf.math.reduce_sum(tf.keras.losses.mean_squared_error(y_true, y_pred))
+        # --------------------------------------------------------------------------------
+        # Localisation loss via MSE sum(T - Y)^2 / N
+        # mean(
+        #   (x_pred-x_true)^2 +
+        #   (x_pred-x_true)^2 +
+        #   (sqrt(w_pred)-sqrt(w_true))^2 +
+        #   (sqrt(h_pred)-sqrt(h_true))^2
+        # )
+        # --------------------------------------------------------------------------------
+        localization_loss: tf.Tensor = tf.math.reduce_mean(tf.square(
+            boxes_truth - boxes_predicted
+        ))
+
+        # --------------------------------------------------------------------------------
+        # Classification loss
+        # --------------------------------------------------------------------------------
+
