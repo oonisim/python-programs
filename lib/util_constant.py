9e83856a4cddd44234bbbc935341046017f50aa5
oonisim <oonisim@gmail.com>
2023-03-01 17:22:28 +1100

    TF dataset

--- lib/util_constant.py
+++ lib/util_constant.py
@@ -1,6 +1,12 @@
+"""Constant variable definition module
+"""
 import numpy as np
 
 
+# --------------------------------------------------------------------------------
+# Data Types
+# Be specific with the storage size to avoid unexpected casting/rounding errors.
+# --------------------------------------------------------------------------------
 TYPE_INT = np.int32
 TYPE_LABEL = np.int32
 TYPE_FLOAT = np.float32     # alias of Python float
