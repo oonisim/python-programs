c4aae3a70b45845e17e07ae0da9a1debe1a2dd13
oonisim <oonisim@gmail.com>
2023-05-08 14:45:52 +1000

    AWS comprehend and translate with pytest

--- lib/run_test.sh
+++ lib/run_test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 DIR=$(realpath $(dirname $0))
+export PYTHONPATH="${PYTHONPATH}:${DIR}/lib"
 
-pytest --capture=no -o log_cli=true -s ${DIR}/test/$1
+# pytest --capture=no -o log_cli=true ${DIR}/test/$1
+pytest ${DIR}/test/$1 -v
