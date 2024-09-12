80bc5e35ca28230b9357e277041423b0588395fb
oonisim <oonisim@gmail.com>
2024-09-12 17:26:46 +1000

    Misc

--- transformer/v1/decoder.py
+++ transformer/v1/decoder.py
@@ -27,7 +27,7 @@ from transformer.v1.common import (
 )
 
 
-class EncodeLayer(nn.Module):
+class DecodeLayer(nn.Module):
     """Class to implement Encoder Layer"""
     def __init__(
             self,
@@ -36,7 +36,7 @@ class EncodeLayer(nn.Module):
             d_model: int = DIM_MODEL,
             dtype: Tensor.dtype = TYPE_FLOAT,
             d_ff: int = 2048,
-            do_mask: bool = False,
+            do_mask: bool = True,
             max_time_steps: int = MAX_SEQUENCE_LENGTH,
             bias: bool = True,
             p_drop: float = 0.1,
@@ -56,6 +56,22 @@ class EncodeLayer(nn.Module):
         """
         super().__init__()
 
+        self.layernorm_masked: nn.LayerNorm = nn.LayerNorm(
+            normalized_shape=d_model,
+            eps=eps,
+            dtype=dtype
+        )
+        self.masked_multihead_attention: MultiHeadAttention = MultiHeadAttention(
+            i_layer=i_layer,
+            num_heads=num_heads,
+            d_model=d_model,
+            dtype=dtype,
+            do_mask=do_mask,
+            max_time_steps=max_time_steps,
+            bias=bias,
+        )
+        self.dropout_masked: nn.Module = nn.Dropout(p=p_drop)
+
         self.layernorm_multihead: nn.LayerNorm = nn.LayerNorm(
             normalized_shape=d_model,
             eps=eps,
@@ -66,7 +82,7 @@ class EncodeLayer(nn.Module):
             num_heads=num_heads,
             d_model=d_model,
             dtype=dtype,
-            do_mask=do_mask,
+            do_mask=False,
             max_time_steps=max_time_steps,
             bias=bias,
         )
@@ -89,12 +105,8 @@ class EncodeLayer(nn.Module):
         self.dropout_positionwise: nn.Module = nn.Dropout(p=p_drop)
 
     def forward(self, x: Tensor) -> Tensor:
-        """Encode.
+        """Decode.
         Citation:
-        > The encoder is composed of a stack of N = 6 identical layers. Each layer has
-        > two sub-layers. The first is a multi-head self-attention mechanism, and the
-        > second is a simple, positionwise fully connected feed-forward network.
-
         > We employ a residual connection around each of the two sub-layers, followed
         > by layer normalization. That is, the output of each sub-layer is
         > LayerNorm(x + Sublayer(x)), where Sublayer(x) is the function implemented
@@ -109,42 +121,14 @@ class EncodeLayer(nn.Module):
         Args:
             x: embedding vector of shape (B,T,D)
         """
-        # Update:
-        # Original paper applied Layer Normalization after Residual Connection which is
-        # called Post Normalization. However, recent approach is Pre-Normalization where
-        # LayerNorm is applied before the sub-layer as in https://arxiv.org/pdf/2002.04745.pdf.
-        # See https://youtu.be/kCc8FmEb1nY?t=5729 and
-
-        # --------------------------------------------------------------------------------
-        # Post Normalization in the original paper. Replaced with Pre Norm as in Update:
-        # 1. First sub-layer followed by Dropout before added to sub-layer input x.
-        # 2. Add Residual Connection.
-        # 3. Layer Normalization.
-        # --------------------------------------------------------------------------------
-        # x = self.layernorm_multihead(
-        #     x + self.dropout_multihead(self.multihead_attention(x))
-        # )
-
-        # DO NOT use += as it is in-place operation that can cause back-prop issue.
-        # https://stackoverflow.com/a/68600205/4281353
-        # https://crazyoscarchang.github.io/2018/10/04/in-pytorch-not-the-same/
+        x = x + self.dropout_masked(self.masked_multihead_attention(self.layernorm_masked(x)))
         x = x + self.dropout_multihead(self.multihead_attention(self.layernorm_multihead(x)))
-
-        # --------------------------------------------------------------------------------
-        # Post Normalization in the original paper. Replaced with Pre Norm as in Update:
-        # 1. Second sub-layer followed by Dropout before added to sub-layer input x.
-        # 2. Add Residual Connection.
-        # 3. Layer Normalization.
-        # --------------------------------------------------------------------------------
-        # x = self.layernorm_positionwise(
-        #     x + self.dropout_positionwise(self.positionwise_feedforward(x))
-        # )
         x = x + self.dropout_positionwise(self.positionwise_feedforward(self.layernorm_positionwise(x)))
         assert torch.all(torch.isfinite(x))
         return x
 
 
-class Encoder(nn.Module):
+class Decoder(nn.Module):
     """Class to implement Transformer Encoder.
     Citation:
     > In addition, we apply dropout to the sums of the embeddings and
@@ -165,7 +149,7 @@ class Encoder(nn.Module):
             d_model: int = DIM_MODEL,
             dtype: Tensor.dtype = TYPE_FLOAT,
             d_ff: int = 2048,
-            do_mask: bool = False,
+            do_mask: bool = True,
             max_time_steps: int = MAX_SEQUENCE_LENGTH,
             bias: bool = True,
             p_drop: float = 0.1,
@@ -183,7 +167,7 @@ class Encoder(nn.Module):
         #     embedding_dim=d_model
         # )
         # initialize_weights(module=self.embedding)
-        self.input_embedding: InputEmbedding = InputEmbedding(
+        self.output_embedding: InputEmbedding = InputEmbedding(
             d_model=d_model,
             vocabulary_size=vocabulary_size,
             dtype=dtype
@@ -214,10 +198,10 @@ class Encoder(nn.Module):
         # --------------------------------------------------------------------------------
         self.dropout: nn.Dropout = nn.Dropout(p=p_drop)
         # --------------------------------------------------------------------------------
-        # Encoder layers
+        # Decoder layers
         # --------------------------------------------------------------------------------
         self.layers: nn.ModuleList = nn.ModuleList([
-            EncodeLayer(
+            DecodeLayer(
                 i_layer=_layer,
                 num_heads=num_heads, d_model=d_model, dtype=dtype, d_ff=d_ff,
                 do_mask=do_mask, max_time_steps=max_time_steps, bias=bias,
@@ -236,7 +220,7 @@ class Encoder(nn.Module):
         # --------------------------------------------------------------------------------
         # Input Embeddings multiplied by sqrt(d_model).
         # --------------------------------------------------------------------------------
-        x = self.input_embedding(indices=indices)
+        x = self.output_embedding(indices=indices)
         assert x.shape == (_B, _T, self.D)
 
         # --------------------------------------------------------------------------------
